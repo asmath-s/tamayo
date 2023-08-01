e4cab5190b24ba0908147cc596211f0a1e19b494
Asmaths <asmath3026@gmail.com>
2023-08-01 19:29:53 +0530

    update

---
+++ assets/js/jquery.js
@@ -0,0 +1,14 @@
+$(".campus-experience-slider").owlCarousel({
+  items: 1,
+  loop: true,
+  margin: 0,
+  nav: false,
+  dots: false,
+  autoplay: true,
+  smartSpeed: 1000,
+  autoplayHoverPause: true,
+  navText: [
+    "<i class='ri-arrow-left-line'></i>",
+    "<i class='ri-arrow-right-line'></i>",
+  ],
+});
