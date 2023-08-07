1f81c835c0077fb7e0b7ca404bc5f93038390020
Asmath.s <asmath3026@gmail.com>
2023-08-07 20:34:29 +0530

    updete

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,14 +1,28 @@
-$(".campus-experience-slider").owlCarousel({
+$(".testimonial-carousel").owlCarousel({
   items: 1,
   loop: true,
   margin: 0,
-  nav: false,
+  nav: true,
   dots: false,
   autoplay: true,
   smartSpeed: 1000,
   autoplayHoverPause: true,
   navText: [
-    "<i class='ri-arrow-left-line'></i>",
-    "<i class='ri-arrow-right-line'></i>",
+    "<i class='fa-solid fa-angle-left'></i>",
+    "<i class='fa-solid fa-angle-right'></i>",
+  ],
+});
+$(".facilitiescarousel").owlCarousel({
+  items: 3,
+  loop: true,
+  margin: 0,
+  nav: true,
+  dots: false,
+  autoplay: true,
+  smartSpeed: 1000,
+  autoplayHoverPause: true,
+  navText: [
+    "<i class='fa-solid fa-angle-left'></i>",
+    "<i class='fa-solid fa-angle-right'></i>",
   ],
 });
