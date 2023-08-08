e1b5df2ef2af7b12e4418f7cd37670932377954d
Asmaths <asmath3026@gmail.com>
2023-08-08 19:35:40 +0530

    update

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,19 +1,20 @@
 $(".testimonial-carousel").owlCarousel({
   items: 1,
   loop: true,
-  margin: 0,
+  margin: 100,
   nav: true,
   dots: false,
   autoplay: true,
   smartSpeed: 1000,
   autoplayHoverPause: true,
+  touchDrag: true,
+  mouseDrag: true,
   navText: [
     "<i class='fa-solid fa-angle-left'></i>",
     "<i class='fa-solid fa-angle-right'></i>",
   ],
 });
 $(".facilitiescarousel").owlCarousel({
-  items: 3,
   loop: true,
   margin: 0,
   nav: true,
@@ -21,8 +22,22 @@ $(".facilitiescarousel").owlCarousel({
   autoplay: true,
   smartSpeed: 1000,
   autoplayHoverPause: true,
+  touchDrag: true,
+  mouseDrag: true,
   navText: [
     "<i class='fa-solid fa-angle-left'></i>",
     "<i class='fa-solid fa-angle-right'></i>",
   ],
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
 });
