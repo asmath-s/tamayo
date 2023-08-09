b5963a185454163fde075e91fac1ccf4797f1ee9
Asmaths <asmath3026@gmail.com>
2023-08-09 18:25:12 +0530

    update

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -44,7 +44,7 @@ $(".facilitiescarousel").owlCarousel({
 $(".studySlide-carousel").owlCarousel({
   loop: true,
   margin: 20,
-  nav: true,
+  nav: false,
   dots: false,
   autoplay: true,
   smartSpeed: 1000,
@@ -65,6 +65,60 @@ $(".studySlide-carousel").owlCarousel({
     },
     1000: {
       items: 3,
+    },
+  },
+});
+$(".accredit-carousel").owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: false,
+  dots: false,
+  autoplay: true,
+  smartSpeed: 1000,
+  autoplayHoverPause: true,
+  touchDrag: true,
+  mouseDrag: true,
+  navText: [
+    "<i class='fa-solid fa-angle-left'></i>",
+    "<i class='fa-solid fa-angle-right'></i>",
+  ],
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 2,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 4,
+    },
+  },
+});
+$(".associates-carousel").owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: false,
+  dots: false,
+  autoplay: true,
+  smartSpeed: 1000,
+  autoplayHoverPause: true,
+  touchDrag: true,
+  mouseDrag: true,
+  navText: [
+    "<i class='fa-solid fa-angle-left'></i>",
+    "<i class='fa-solid fa-angle-right'></i>",
+  ],
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 2,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 4,
     },
   },
 });
