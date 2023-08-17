98feca7250b8ea876640d0927c42742a6c1f2265
Asmath.s <asmath3026@gmail.com>
2023-08-17 15:34:12 +0530

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,52 @@
+var script = document.createElement("script");
+script.async = true;
+script.src = "https://www.googletagmanager.com/gtag/js?id=G-6H3FPE5JJG";
+document.head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "G-6H3FPE5JJG");
+
+// Google claarity
+(function (c, l, a, r, i, t, y) {
+  c[a] =
+    c[a] ||
+    function () {
+      (c[a].q = c[a].q || []).push(arguments);
+    };
+  t = l.createElement(r);
+  t.async = 1;
+  t.src = "https://www.clarity.ms/tag/" + i;
+  y = l.getElementsByTagName(r)[0];
+  y.parentNode.insertBefore(t, y);
+})(window, document, "clarity", "script", "h3ui84blyi");
+
+(function (w, d) {
+  w.CollectId = "6460e2081b0491e28f6198a0";
+  var h = d.head || d.getElementsByTagName("head")[0];
+  var s = d.createElement("script");
+  s.setAttribute("type", "text/javascript");
+  s.async = true;
+  s.setAttribute("src", "https://collectcdn.com/launcher.js");
+  h.appendChild(s);
+})(window, document);
+
+// Google Tag Manager
+(function (w, d, s, l, i) {
+  w[l] = w[l] || [];
+  w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+  var f = d.getElementsByTagName(s)[0],
+    j = d.createElement(s),
+    dl = l != "dataLayer" ? "&l=" + l : "";
+  j.async = true;
+  j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+  f.parentNode.insertBefore(j, f);
+})(window, document, "script", "dataLayer", "GTM-WWNHXHN");
+// End Google Tag Manager
+
 // Show/hide navbar when toggler button is clicked
 function toggleNavbar() {
   const navbarLinks = document.getElementById("navbar-links");
--- page-home.php
+++ page-home.php
@@ -1,4 +1,121 @@
-<?php get_header();?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Dr.Jose G.Tamayo Medical University, Philippines</title>
+  <link rel="shortcut icon" href="<?php bloginfo('template_directory');?>/assets/images/favicon.png"
+    type="image/x-icon" />
+     <link href="https://docs.google.com/document/d/1-IyBCJDZ9yYQ_KtsbOtdp_-57r--3XAcC2hOfa_Ryzw/edit?usp=share_link
+"/>
+<script src="<?php echo get_template_directory_uri();?>/assets/js/custom.js"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous" />
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.css"
+      integrity="sha512-WEQNv9d3+sqyHjrqUZobDhFARZDko2wpWdfcpv44lsypsSuMO0kHGd3MQ8rrsBn/Qa39VojphdU6CMkpJUmDVw=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
+      integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"
+      integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <script
+      src="https://kit.fontawesome.com/b0eb9bd990.js"
+      crossorigin="anonymous"
+    ></script>
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/global.css" />
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/header.css" />
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/footer.css" />
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/home.css" />
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/gallery.css" />
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/about.css" />
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/philippines.css" />
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/blog.css"/>
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/contact.css"/>
+
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/form.css"/>
+ 
+
+    
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/responsive.css" />
+
+  </head>
+  <body>
+    <div class="topbar">
+      <div class="container">
+        <div class="topbarinside d-flex align-items-center">
+          <div class="social-media d-flex align-items-center">
+            <h6>Follow Us :</h6>
+            <div class="social-icons d-flex align-items-center">
+              <a href="https://www.facebook.com/UPH.DJGTamayoMedicalUniversity"><i class="fa-brands fa-facebook-f"></i></a>
+              <a href="https://www.instagram.com/uph_djgtamayomedicaluniversity/"><i class="fa-brands fa-instagram"></i></a>
+              <a href="https://twitter.com/UPH_DJGTMU"><i class="fa-brands fa-twitter"></i></a>
+              <a href="https://www.linkedin.com/in/uph-djg-tamayo-medical-university-381807277/"><i class="fa-brands fa-linkedin"></i></a>
+              <a href="https://www.youtube.com/channel/UCK8k9jpB2YzMBiRVi1r1gCw"><i class="fa-brands fa-youtube"></i></a> 
+            </div>
+          </div>
+          <div class="trust d-flex align-items-center">
+            <img src="<?php bloginfo('template_directory');?>/assets/images/trust-icon.webp" alt="" />
+            <h5>Most Premium and Prestigious University</h5>
+          </div>
+          <div class="phone d-flex align-items-center">
+            <a  href="tel:+918122728122">
+              <i class="fa-solid fa-phone"></i>
+              <h5>8122878122</h5>
+            </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <nav>
+    <div class="container d-flex align-items-center navigation">
+
+      <div class="logo">
+        <img src="<?php bloginfo('template_directory');?>/assets/images/Tamayo-logo.webp" alt="Tamayo" />
+      </div>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-links"
+        onclick="toggleNavbar()">
+        <i class="fa-solid fa-bars"></i>
+      </button>
+      <button class="closebtn" type="button" data-toggle="collapse" data-target="#navbar-links"
+        onclick="toggleNavbar()">
+        <i class="fa-solid fa-x"></i>
+      </button>
+      <ul class="navbar navbar-hide" id="navbar-links">
+        <li>
+          <a href="https://www.uphdjgtmedicaluniversity.com/">Home</a>
+        </li>
+        <li>
+          <a href="https://www.uphdjgtmedicaluniversity.com/about">About Us</a> </li>
+        <li><a href="https://uphdjgtmedicaluniversity.com/philippines/">Phillippines</a> </li>
+        <li><a href="https://uphdjgtmedicaluniversity.com/gallery/">Gallery</a> </li>
+        <li><a href="https://uphdjgtmedicaluniversity.com/contact-us/">Contact</a> </li>
+        <li><a href="https://uphdjgtmedicaluniversity.com/blog/">Blog</a> </li>
+        <li>
+          <button type="button" class="btn btn-primary admissionbutton d-flex align-items-center">
+            Admission Now <i class="fa-solid fa-arrow-right"></i>
+          </button>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <div class="overlay"></div>
 
 
     <section class="banner">
@@ -893,5 +1010,105 @@
         </div>
       </div>
     </section>
+<section class="cta">
+  <div class="container">
+    <div class="ctaBox">
+      <div class="ctaContent">
+        <p>Looking for the Best Medical University?</p>
+        <div class="ctaBtn">
+          <a href="https://uphdjgtmedicaluniversity.com/contact-us/">
+            <button type="button" class="ContactBtn">
+              <span>
+                Contact Us
+                <i class="fa-solid fa-arrow-right" style="color: #000000; padding-left: 15px"></i></span>
+            </button>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 
-<?php get_footer();?>
+<footer class="footer-area ptb-70">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4 col-md-6 footerSide1">
+        <div class="footerSide1Box">
+          <a href="#" class="footerLogo">
+            <img src="<?php bloginfo('template_directory');?>/assets/images/Tamayo-logo footer.png" />
+          </a>
+          <p>
+            Empowering Future Healthcare Leaders with Excellence in
+            Education : UPH-DJG Tamayo Medical University.
+          </p>
+          <div class="readMoreBtn">
+            <a href="https://www.uphdjgtmedicaluniversity.com/about">
+              <button type="button" class="ContactBtn">
+                <span> Read Us </span>
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 footerSide2">
+        <div class="offialInfo">
+          <h3>Official Info</h3>
+          <a href="#"><i class="fa-solid fa-envelope"
+              style="color: #ffffff"></i><span>uphdjgtmedicaluniversity@gmail.com</span></a>
+          <a href="tel:+918122878122"><i class="fa-solid fa-phone"
+              style="color: #ffffff"></i><span>8122878122</span></a>
+          <h3>Open Hours</h3>
+          <p>Mon - Fri: 09 AM - 06 PM,</p>
+          <p>Saturday & Sunday: 09 AM - 01 PM</p>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 footerContactform">
+        <div class="Contactform1">
+          <h5>Contact Us</h5>
+          <?php echo do_shortcode('[contact-form-7 id="73607cb" title="Footer form"]'); ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+<div class="copy-right-area">
+  <div class="container">
+    <p>
+      2023
+      <i class="fa-regular fa-copyright"></i>
+      All Copyrights Reversed by UPH-Dr. Jose G. Tamayo Medical University
+    </p>
+  </div>
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"
+  integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ=="
+  crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="<?php echo get_template_directory_uri();?>/assets/js/lazyload.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"
+  integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
+  crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.js"
+  integrity="sha512-C1zvdb9R55RAkl6xCLTPt+Wmcz6s+ccOvcr6G57lbm8M2fbgn2SUjUJbQ13fEyjuLViwe97uJvwa1EUf4F1Akw=="
+  crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"
+  integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
+  crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/masonry/4.2.2/masonry.pkgd.min.js"
+  integrity="sha512-JRlcvSZAXT8+5SQQAvklXGJuxXTouyq8oIMaYERZQasB8SBDHZaUbeASsJWpk0UUrf89DP3/aefPPrlMR1h1yQ=="
+  crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<script src="<?php echo get_template_directory_uri();?>/assets/js/jquery.js"></script>
+<script src="<?php echo get_template_directory_uri();?>/assets/js/custom.js"></script>
+
+</body>
+
+</html>
