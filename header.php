ecc1babea0280ecd9dd6ea3b2ba5c765cf42ae95
Asmath.s <asmath3026@gmail.com>
2023-08-17 13:13:17 +0530

    update

--- header.php
+++ header.php
@@ -43,8 +43,9 @@
     <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/home.css" />
     <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/about.css" />
     <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/philippines.css" />
-    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/blog.css" />
-    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/form.css" />
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/blog.css"/>
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/form.css"/>
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/gallery.css"/>
     <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/responsive.css" />
 
   </head>
