fd50993449f846b794b23179242e8f54ed9be834
Asmaths <asmath3026@gmail.com>
2023-08-19 20:27:45 +0530

    update

--- footer.php
+++ footer.php
@@ -20,7 +20,7 @@
 <footer class="footer-area ptb-70">
   <div class="container">
     <div class="row">
-      <div class="col-lg-4 col-md-6 footerSide1">
+      <div class="col-lg-3 col-md-6 footerSide1">
         <div class="footerSide1Box">
           <a href="https://www.uphdjgtmedicaluniversity.com/" class="footerLogo">
             <img src="<?php bloginfo('template_directory');?>/assets/images/Tamayo-logo footer.png" alt="Dr. Jose G. Tamayo Medical University in Manila, Philippines"/>
@@ -29,30 +29,49 @@
             Empowering Future Healthcare Leaders with Excellence in
             Education : UPH-DJG Tamayo Medical University.
           </p>
-          <div class="readMoreBtn">
-            <a href="https://www.uphdjgtmedicaluniversity.com/about">
-              <button type="button" class="ContactBtn">
-                <span> Read Us </span>
-              </button>
-            </a>
-          </div>
+          
+          <div class="social-icons-footer d-flex align-items-center">
+             <a href="https://www.facebook.com/UPH.DJGTamayoMedicalUniversity"><i
+                 class="fa-brands fa-facebook-f"></i></a>
+             <a href="https://www.instagram.com/uph_djgtamayomedicaluniversity/"><i
+                 class="fa-brands fa-instagram"></i></a>
+             <a href="https://twitter.com/UPH_DJGTMU"><i class="fa-brands fa-twitter"></i></a>
+             <a href="https://www.linkedin.com/in/uph-djg-tamayo-medical-university-381807277/"><i
+                 class="fa-brands fa-linkedin"></i></a>
+             <a href="https://www.youtube.com/channel/UCK8k9jpB2YzMBiRVi1r1gCw"><i class="fa-brands fa-youtube"></i></a>
+           </div>
         </div>
       </div>
-
-      <div class="col-lg-4 col-md-6 footerSide2">
+      <div class="col-lg-3 col-md-6 ">
         <div class="offialInfo">
-          <h3>Official Info</h3>
-          <a href="mailto:uphdjgtmedicaluniversity@gmail.com"><i class="fa-solid fa-envelope"
-              style="color: #ffffff"></i><span>uphdjgtmedicaluniversity@gmail.com</span></a>
-          <a href="tel:+918122878122"><i class="fa-solid fa-phone"
-              style="color: #ffffff"></i><span>8122878122</span></a>
-          <h3>Open Hours</h3>
-          <p>Mon - Fri: 09 AM - 06 PM,</p>
-          <p>Saturday & Sunday: 09 AM - 01 PM</p>
+            <h3>Official Info</h3>
+            <a href="mailto:uphdjgtmedicaluniversity@gmail.com"><i class="fa-solid fa-envelope"
+                style="color: #ffffff"></i><span>uphdjgtmedicaluniversity@gmail.com</span></a>
+            <a href="tel:+918122878122"><i class="fa-solid fa-phone"
+                style="color: #ffffff"></i><span>8122878122</span></a>
+            <h3>Open Hours</h3>
+            <p>Mon - Fri: 09 AM - 06 PM,</p>
+            <p>Saturday & Sunday: 09 AM - 01 PM</p>
+          </div>
+      </div>
+      <div class="col-lg-3 col-md-6 footerSide2">
+        <div class="quickLinks">
+          <h3>Quick link</h3>
+          <ul class="quicklinks-line" id="">
+            <li>
+              <a href="https://www.uphdjgtmedicaluniversity.com/">Home</a>
+            </li>
+            <li>
+              <a href="https://uphdjgtmedicaluniversity.com/about-us/">About Us</a> </li>
+            <li><a href="https://uphdjgtmedicaluniversity.com/mbbs-in-philippines/">MBBS in Phillippines</a> </li>
+            <li><a href="https://uphdjgtmedicaluniversity.com/gallery/">Gallery</a> </li>
+            <li><a href="https://uphdjgtmedicaluniversity.com/contact-us/">Contact</a> </li>
+            <li><a href="https://uphdjgtmedicaluniversity.com/blog/">Blog</a> </li>
+          </ul>
         </div>
       </div>
 
-      <div class="col-lg-4 col-md-6 footerContactform">
+      <div class="col-lg-3 col-md-6 footerContactform">
         <div class="Contactform1">
           <h5>Contact Us</h5>
           <?php echo do_shortcode('[contact-form-7 id="73607cb" title="Footer form"]'); ?>
